eeae90625cb820bdec84f59447d9e869da1d56f7
张大千 <zhangdaqian@iqidao.com>
2020-03-07 15:33:40 +0800

    first commit

--- FileLogger_test.go
+++ FileLogger_test.go
@@ -3,18 +3,22 @@ package logger_test
 import (
 	"errors"
 	"testing"
+	"time"
 
-	"github.com/RocksonZeta/summer/logger"
+	"github.com/RocksonZeta/logger"
 )
+
+var serviceLog = logger.NewLogger(logger.Options{Level: "debug", File: "test_log.%Y%m%d.log", ForceNewFile: true, MaxAge: 7 * 24 * time.Hour})
+var log = serviceLog.Fork("good/service/users", "Users")
+
+func TestLog(t *testing.T) {
+	log.Trace().Func("TestWrite").Interface("user", User{Id: 1}).Send()
+	log.Debug().Func("TestWrite").Str("sql", "select * from User where id=?").Int("id", 1).Send()
+	log.Info().Func("Signin").Str("uid", "1").Msg("hello")
+	log.Warn().Func("Signin").Str("uid", "1").Msg("bad user")
+	log.Error().Stack().Err(errors.New("oh no")).Send()
+}
 
 type User struct {
 	Id int
-}
-
-func TestWrite(t *testing.T) {
-	logger := logger.NewLogger(logger.Options{Level: "debug", File: "test_log.%Y%m%d.log", ForceNewFile: true})
-	log := logger.Fork("logger_test", "")
-	log.Debug().Func("TestWrite").Interface("obj", User{Id: 1}).Msg("hello1")
-	log.Info().Func("hello").Str("uid", "1").Msg("hello")
-	log.Error().Stack().Err(errors.New("hello1")).Send()
 }
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # logger
-combine  zerolog and file-rotatelogs.
+combine zerolog and file-rotatelogs.
+
+## Install
+```
+go get -u github.com/RocksonZeta/logger
+```
+
+## Example
+```go
+package logger_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/RocksonZeta/logger"
+)
+
+var serviceLog = logger.NewLogger(logger.Options{Level: "debug", File: "test_log.%Y%m%d.log", ForceNewFile: true, MaxAge: 7 * 24 * time.Hour})
+var log = serviceLog.Fork("good/service/users", "Users")
+
+func TestLog(t *testing.T) {
+	log.Trace().Func("TestWrite").Interface("user", User{Id: 1}).Send()
+	log.Debug().Func("TestWrite").Str("sql", "select * from User where id=?").Int("id", 1).Send()
+	log.Info().Func("Signin").Str("uid", "1").Msg("hello")
+	log.Warn().Func("Signin").Str("uid", "1").Msg("bad user")
+	log.Error().Stack().Err(errors.New("oh no")).Send()
+}
+
+type User struct {
+	Id int
+}
+```
